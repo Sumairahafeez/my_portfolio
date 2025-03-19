9e5182a2c8f89bdc29ed92548fc840a68f078a5e
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2025-03-19 20:29:41 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'sumairahafeezfp@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/Sumairahafeez',
     },
     {
       name: 'Instagram',
