950f38185312888028c968d00a52a9ab35a17f58
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2025-03-23 16:05:46 +0500

    no

--- gatsby-node.js
+++ gatsby-node.js
@@ -66,7 +66,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   });
 };
 
-// https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
+https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
   if (stage === 'build-html' || stage === 'develop-html') {
@@ -105,3 +105,4 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     },
   });
 };
+
